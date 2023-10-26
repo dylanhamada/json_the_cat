5e6c09720b9fee4201e22e9c55bec852fcbf6969
dylanhamada <dylan.hamada@gmail.com>
2023-10-26 18:34:57 -0400

    Write breedFetcherTest
    - Create breedFetcherTest.js and write two tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,23 @@
 const request = require("request");
 
-const userInput = process.argv[2];
-
-const catReq = (cat) => {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${cat}`, (err, resp, body) => {
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, resp, body) => {
     // if there is a request error, print an error message and exit function
     if (err) {
-      return console.log(`Error: ${err}`);
+      callback(err, null);
+      return;
     }
 
     // if body is more than an empty array, parse it and print breed description
     if (body.length > 2) {
       const data = JSON.parse(body);
-      return console.log(data[0].description);
+      callback(null, data[0].description);
+      return;
+    } else {
+      callback(null, `Sorry, that breed can't be found.`);
+      return;
     }
-
-    // otherwise, print a message that the breed could not be found
-    return console.log(`Sorry, the ${cat} breed cannot be found.`);
   });
 };
 
-catReq(userInput);
+module.exports = { fetchBreedDescription };
